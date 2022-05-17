0e932ca90d5497f3aabe2b740a48385c0c02ccfc
MakolkinDaniil <77923294+MakolkinDaniil@users.noreply.github.com>
2022-05-17 16:06:34 +0700

    SkatingSystem

---
+++ SkatingSystem/SkatingSystem/Forms/Clubs.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SkatingSystem.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для Clubs.xaml
+    /// </summary>
+    public partial class Clubs : Window
+    {
+        public Clubs()
+        {
+            InitializeComponent();
+            TestDG.ItemsSource = SkatingEntities.GetContext().Kluby.ToList();
+            SkatingEntities db = new SkatingEntities();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            Hide();
+        }
+
+        // Получаем данные из таблицы и записываем в TextBox.
+        private void TestDG_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Kluby path = TestDG.SelectedItem as Kluby;
+            NazvanieTB.Text = path.Nazvanie;
+            GorodTB.Text = path.Gorod;
+            StranaTB.Text = path.Strana;
+            FIOTreneraTB.Text = path.FIOTrenera;
+        }
+    }
+}
---
+++ SkatingSystem/SkatingSystem/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SkatingSystem
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Dancers_Click(object sender, RoutedEventArgs e)
+        {
+            Forms.Dancers dancers = new Forms.Dancers();
+            dancers.Show();
+            Hide();
+        }
+
+        private void Judges_Click(object sender, RoutedEventArgs e)
+        {
+            Forms.Judges judges = new Forms.Judges();
+            judges.Show();
+            Hide();
+        }
+
+        private void Clubs_Click(object sender, RoutedEventArgs e)
+        {
+            Forms.Clubs clubs = new Forms.Clubs();
+            clubs.Show();
+            Hide();
+        }
+
+        private void Groups_Click(object sender, RoutedEventArgs e)
+        {
+            Forms.Groups groups = new Forms.Groups();
+            groups.Show();
+            Hide();
+        }
+    }
+}
